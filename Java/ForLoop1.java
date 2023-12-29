06fab012a6c7c39211bcd90604c136cdfdf47187
SujithAshok <142969983+SujithAshok@users.noreply.github.com>
2023-12-29 08:25:42 +0530

    Add files via upload

---
+++ Java/ForLoop1.java
@@ -0,0 +1,15 @@
+class ForLoop1
+{
+public static void main(String args[])
+{
+for(int i=0; i<=10; i++)
+{
+System.out.println(i);
+}
+System.out.println(" ");
+for(int j=25; j<=35; j++)
+{
+System.out.println(j);
+}
+}
+}
