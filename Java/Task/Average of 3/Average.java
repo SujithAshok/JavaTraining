4c1838a78c606953e2bfb8c46bfdd2fc06528f09
SujithAshok <142969983+SujithAshok@users.noreply.github.com>
2024-01-15 22:08:08 +0530

    Add files via upload

---
+++ Java/Task/Average of 3/Average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Average
+{
+public static void main(String args[])
+{
+	
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+	int avg ;
+	avg = (a+b+c)/2;
+	System.out.print("The average of a,b,c is: " +avg);
+}
+ }
