f5fb256b28c86098b2c595b16f9f848d0bb21d3b
SujithAshok <142969983+SujithAshok@users.noreply.github.com>
2024-02-14 14:33:43 +0530

    Add files via upload

---
+++ Java/Task/2 arguments from command line arguments(name age)/CommandLine.java
@@ -0,0 +1,14 @@
+public class CommandLine
+ {
+    public static void main(String[] args) 
+	{
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+        if (age >= 18) 
+		{
+            System.out.println(name + " is eligible to vote");
+        } else {
+            System.out.println(name + " is not eligible to vote");
+        }
+    }
+}
